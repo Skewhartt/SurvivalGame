69d27b6a793d3a18a444be9b787bced3bc68cd4f
Skewhartt <pinchalix@gmail.com>
2022-07-11 21:57:11 +0200

    Avancement sur le système d'inventaire

--- Assets/Scripts/Inventory.cs
+++ Assets/Scripts/Inventory.cs
@@ -1,14 +1,80 @@
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Inventory : MonoBehaviour
 {
     [SerializeField]
     private List<ItemData> content = new List<ItemData>();
 
+    [SerializeField]
+    private GameObject inventoryPanel;
+
+    [SerializeField]
+    private Transform inventorySlotsParent;
+
+    [SerializeField]
+    private Sprite transparentTexture;
+
+
+    private void Start ()
+    {
+        inventoryPanel.SetActive(false);
+        RefreshContent();
+    }
+
+    private void Update ()
+    {
+        if (Input.GetKeyDown(KeyCode.I))
+        {
+            RefreshContent();
+            inventoryPanel.SetActive(!inventoryPanel.activeSelf);
+        }
+
+        if (GetContentCount() > GetInventorySlotCount())
+        {
+            content.RemoveAt(inventorySlotsParent.childCount);
+        }
+    }
 
     public void AddItem (ItemData item)
     {
         content.Add(item);
+        RefreshContent();
+    }
+
+    public void CloseInventory ()
+    {
+        inventoryPanel.SetActive(false);
+    }
+
+    public int GetContentCount ()
+    {
+        return content.Count;
+    }
+
+    public int GetInventorySlotCount ()
+    {
+        return inventorySlotsParent.childCount;
+    }
+
+    private void RefreshContent ()
+    {
+        //for (int i = 0; i < content.Count; i++)
+        //{
+        //    inventorySlotsParent.GetChild(i).GetChild(0).GetComponent<Image>().sprite = content[i].visual;
+        //}
+
+        for (int i = 0; i < GetInventorySlotCount(); i++)
+        {
+            if (i < GetContentCount())
+            {
+                inventorySlotsParent.GetChild(i).GetChild(0).GetComponent<Image>().sprite = content[i].visual;
+            } else
+            {
+                inventorySlotsParent.GetChild(i).GetChild(0).GetComponent<Image>().sprite = transparentTexture;
+            }
+            
+        }
     }
 }
--- Assets/Scripts/PickupItem.cs
+++ Assets/Scripts/PickupItem.cs
@@ -5,6 +5,9 @@ public class PickupItem : MonoBehaviour
     [SerializeField]
     private float pickupRange = 2.6f;
 
+    [SerializeField]
+    private Inventory inventory;
+
     public PickupBehaviour playerPickupBehaviour;
 
     // Update is called once per frame
@@ -16,9 +19,9 @@ public class PickupItem : MonoBehaviour
         {
             if (hit.transform.CompareTag("Item"))
             {
-                Debug.Log("Il y a un item");
+                // Debug.Log("Il y a un item");
 
-                if (Input.GetKeyDown(KeyCode.E))
+                if (Input.GetKeyDown(KeyCode.E) && inventory.GetContentCount() < inventory.GetInventorySlotCount())
                 {
                     playerPickupBehaviour.DoPickup(hit.transform.gameObject.GetComponent<Item>());
                 }
